73f349e1150e5bdbc5e009220f77bdbf5dd8311a
Kynuak <bapt.trouille@gmail.com>
2023-10-20 18:47:07 +0200

    C'etait dur

---
+++ index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en" data-theme="light">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Test Pico</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@1/css/pico.min.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <nav >
+        <hgroup><h4>Discovering Pico</h4></hgroup>
+        
+        <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="">About</a></li>
+            <li>
+                <details role="list">
+                    <summary aria-haspopup="listbox" role="link"><a>Categories</a></summary>
+                    <ul role="listbox">
+                        <li><a>Photos</a></li>
+                        <li><a>Articles</a></li>
+                        <li><a>Projects</a></li>
+                    </ul>
+                </details>
+            </li>
+        </ul>
+    </nav>
+    <main class="container-fluid">
+                <hgroup>
+                    <h2>Journey into Pico.css</h2>
+                    <h3><a href="">https://picocss.com</a></h3>
+                </hgroup>
+
+        <section class="grid">
+            <article>
+                <header><img src="https://picsum.photos/id/193/4000/3000"></header>
+                <div class="body_card">
+                    <h5>Visit Components classless</h5>
+                    <p>Some quick example text to build on the card title and make up the builk of the card's content</p>
+                    </div>
+                <footer>Read more</footer>
+            </article>
+            <article>
+                <header><img src="https://picsum.photos/id/96/4000/3000"></header>
+                <div class="body_card">
+                    <h5>Facing Casacade Style Sheet</h5>
+                    <p>Some quick example text.</p>
+                </div>
+                <footer>Read more</footer>
+            </article>
+            <article>
+                <header><img src="https://picsum.photos/id/199/4000/3000"></header>
+                <div class="body_card">
+                    <h5>Contemplate Form Inputs river</h5>
+                    <p>Make up the bulk of the card's content.</p>
+                </div>
+                <footer>Read more</footer>
+            </article>
+            <article>
+                <header><img src="https://picsum.photos/id/103/4000/3000"></header>
+                <div class="body_card">
+                    <h5>Enjoy no Javascript</h5>
+                    <p>Build on the card title and make up the bulk of the card's content.</p>
+                </div>
+                <footer>Read more</footer>
+            </article>
+        </section>
+
+    </main>
+        <section class="container-fluid">
+                <h3>The css frameworks are very useful</h3>
+                <h4>Learning them comes with practice</h4>
+        </section>
+    <footer>
+        © 2023 wilders | images ©picsum
+    </footer>
+</body>
+</html>
